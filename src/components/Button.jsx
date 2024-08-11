6ca9641e994e462982439eb2f8837a06b8e9fb2d
INTSEAN <119046561+INTSEAN@users.noreply.github.com>
2024-08-10 23:07:30 -0400

    Blue UI to Green UI

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 export default function Workout(props) {
-  const {text, func} = props
+  const { text, func } = props;
   return (
     <>
-      <button onClick={func} className="px-8 py-4 mx-auto rounded-medium border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200"><p>{text}</p></button>
+      <button
+        onClick={func}
+        className="px-8 py-4 mx-auto rounded-medium border-[2px] bg-slate-950 border-green-400 border-solid greenShadow duration-200"
+      >
+        <p>{text}</p>
+      </button>
     </>
-  )
+  );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,21 +4,33 @@ import Button from "./Button";
 export default function Hero() {
   return (
     <>
-      <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center
-       max-w-[800px] w-full mx-auto p-4'>
+      <div
+        className="min-h-screen flex flex-col gap-10 items-center justify-center text-center
+       max-w-[800px] w-full mx-auto p-4"
+      >
         <div className="flex flex-col gap-4">
-          <p>IT'S TIME TO GET</p>
-          <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl"> 
-            GYM <span className="text-blue-400">JAM</span>
+          <p>IT'S TIME FOR</p>
+          <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+            GYM <span className="text-green-400">JAM</span>
           </h1>
         </div>
-        <p className="text-sm md:text-base font-light">I hereby acknowledge that I may become unbelievably amazed by the 
-          music <span className="text-blue-400 font-medium">jams</span> during my <span className="text-blue-400 font-medium">gym</span> experience and accept all risks of becoming 
-          entertained, and plagued by not being able to <span className="text-blue-400 font-medium">resist dancing in front of other people.</span></p>
-          <Button func={() => {
-            window.location.href = '#generate'
-          }} text="Accept and Begin"></Button>
+        <p className="text-sm md:text-base font-light">
+          I hereby acknowledge that I may become unbelievably amazed by the
+          music <span className="text-green-400 font-medium">jams</span> during
+          my <span className="text-green-400 font-medium">gym</span> experience
+          and accept all risks of becoming entertained, and plagued by not being
+          able to{" "}
+          <span className="text-green-400 font-medium">
+            resist dancing in front of other people.
+          </span>
+        </p>
+        <Button
+          func={() => {
+            window.location.href = "#generate";
+          }}
+          text="Accept and Begin"
+        ></Button>
       </div>
     </>
-  )
+  );
 }
